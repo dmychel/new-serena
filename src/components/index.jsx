d2cad35a1407a24171915a9334f3c1622d3d9547
dmychel <dmychel@tutanota.com>
2025-03-12 16:04:50 -0400

    added bases for scrolling carousel

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,10 +3,13 @@ import styles from "/src/styles/index.module.scss";
 export default function Index() {
   return (
     <div className={styles.index}>
-      <div className="carousel">
-        <img src="https://placehold.co/600x400" alt="placeholder" />
-        <img src="https://placehold.co/600x400" alt="placeholder" />
-        <img src="https://placehold.co/600x400" alt="placeholder" />
+      <div className={styles.carousel}>
+        <div className={styles.img_container}>
+          <img src="https://placehold.co/600x400" alt="placeholder" />
+        </div>
+        <div className={styles.img_container}>
+          <img src="https://placehold.co/600x400" alt="placeholder" />
+        </div>
       </div>
     </div>
   );
