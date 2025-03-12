77cec1226805beffcba594dad4428dd633d0161a
dmychel <dmychel@tutanota.com>
2025-03-12 16:38:21 -0400

    import header

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./index.css";
 import Index from "./components";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
