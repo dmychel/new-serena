91ceb2120c0d288c57b783ea86dd03af1f04191f
dmychel <dmychel@tutanota.com>
2025-03-12 17:10:01 -0400

    update index import route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Index from "./components";
+import Index from "./routes/Index.jsx";
 import Header from "./components/Header";
 
 function App() {
