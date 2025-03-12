3df73be114c20cadad830ebde0ec7446d4198868
dmychel <dmychel@tutanota.com>
2025-03-12 17:01:30 -0400

    header base

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header>
-      <h1>New Serena</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">New Serena</Link>
+          </li>
+          <div>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/">Menu</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
     </header>
   );
 }
