77fe4193f3479a3724d69b4843c80d34733011f0
dmychel <dmychel@tutanota.com>
2025-03-12 15:53:37 -0400

    import styles

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,13 @@
+import styles from "/src/styles/index.module.scss";
+
 export default function Index() {
-  return <h1>Index Page</h1>;
+  return (
+    <div className={styles.index}>
+      <div className="carousel">
+        <img src="https://placehold.co/600x400" alt="placeholder" />
+        <img src="https://placehold.co/600x400" alt="placeholder" />
+        <img src="https://placehold.co/600x400" alt="placeholder" />
+      </div>
+    </div>
+  );
 }
