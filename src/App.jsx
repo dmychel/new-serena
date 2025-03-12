a4834945a069e6269195e6cf253df93f446df27d
dmychel <dmychel@tutanota.com>
2025-03-12 15:39:20 -0400

    react router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./index.css";
+import Index from "./components";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
